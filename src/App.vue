d3ddce56f1c2c71da72a5a4c3d222e21ba8c4ff4
wangyuan198 <wangyuan198@jd.com>
2022-10-18 14:31:44 +0800

    chore: init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{user.name}}</li>
+  </ul>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(response =>{
+      this.users=response.data
+    })
   }
 }
 </script>
